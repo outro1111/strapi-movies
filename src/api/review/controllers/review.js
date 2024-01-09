c2674f0118b6ec290b6d75e965b7c143e8f4b771
DESKTOP-FVEKAVA\CJ <outro1111@naver.com>
2024-01-09 12:44:48 +0900

    controller테스트

--- src/api/review/controllers/review.js
+++ src/api/review/controllers/review.js
@@ -6,4 +6,8 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::review.review');
+module.exports = createCoreController('api::review.review', ({ strapi }) => ({
+  async create(ctx) {
+    console.log(ctx);
+  },
+}));

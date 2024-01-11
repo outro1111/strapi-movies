2cae995548b2f5aa78a8d63af0a6e28df72933b8
DESKTOP-FVEKAVA\CJ <outro1111@naver.com>
2024-01-11 16:15:29 +0900

    리뷰 중복 체크

--- src/api/review/controllers/review.js
+++ src/api/review/controllers/review.js
@@ -4,6 +4,30 @@
  * review controller
  */
 
+const { errors } = require('@strapi/utils');
+const { ApplicationError } = errors;
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::review.review');
+module.exports = createCoreController('api::review.review', ({ strapi }) => ({
+  async create(ctx) {
+    const { user, movie } = ctx.request.body.data;
+    try { // 리뷰 데이터에서 작성 user id를 검색 
+      const existingReview = await strapi.entityService.findMany(
+        'api::review.review',
+        {
+          filters: {
+            movie: movie,
+            user: user
+          },
+        }
+      );
+      if (existingReview.length > 0) { // 작성자가 쓴 리뷰가 있을 시
+        return { error: 'already review' }; // 에러 메시지 return
+      } 
+      const response = await super.create(ctx); // 중복 리뷰가 없을 시 리뷰 작성
+      return response;
+    } catch(error) {
+      return ctx.badRequest('Failed to create review');
+    }
+  },
+}));

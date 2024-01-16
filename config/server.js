c2604d5780b6b2a582e8eb753fbda8f6a88c730e
DESKTOP-FVEKAVA\CJ <outro1111@naver.com>
2024-01-16 10:40:37 +0900

    google sso 복원

--- config/server.js
+++ config/server.js
@@ -1,8 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('', 'https://strapi-movies-production.up.railway.app'), // google sso
-  Proxy: true, // google sso
   app: {
     keys: env.array('APP_KEYS'),
   },

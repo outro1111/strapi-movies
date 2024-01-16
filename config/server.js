481b1e43b0087d5d48649bacf0e06237f8bbca34
DESKTOP-FVEKAVA\CJ <outro1111@naver.com>
2024-01-16 10:27:54 +0900

    google sso

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('', 'https://strapi-movies-production.up.railway.app'),
+  url: env('', 'https://strapi-movies-production.up.railway.app'), // google sso
+  Proxy: true, // google sso
   app: {
     keys: env.array('APP_KEYS'),
   },

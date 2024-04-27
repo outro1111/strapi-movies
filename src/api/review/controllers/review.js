964d75a0b80e759deb683079384d929000ca40fc
outro1111 <67306701+outro1111@users.noreply.github.com>
2024-04-27 21:57:05 +0900

    Update review.js

--- src/api/review/controllers/review.js
+++ src/api/review/controllers/review.js
@@ -20,7 +20,9 @@ module.exports = createCoreController('api::review.review', ({ strapi }) => ({
         }
       );
       if (existingReview.length > 0) { // 작성자가 쓴 리뷰가 있을 시
-        return { error: 'already review' }; // 에러 메시지 return
+        ctx.response.status = -999; // 상태 코드를 custom 코드 -999으로 설정
+        // return { error: 'already review' }; // 에러 메시지 return
+        // return ctx.response.body = { error: 'already review' }; // 에러 메시지 return 다른 방법
       } 
       const response = await super.create(ctx); // 중복 리뷰가 없을 시 리뷰 작성
       return response;
